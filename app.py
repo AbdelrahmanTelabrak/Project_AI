f8f93dd0e06b2cb93d32868d48207b06f4dc890c
Abdelrahmanm22 <abdra1396@gmail.com>
2023-05-13 23:09:03 +0300

    ignore

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 import numpy as np
+import pygame
+import sys
+
 COLUMNS = 7
 ROWS = 6
+blue = (0,0,255)
+black = (0,0,0)
+
 
 def Build_board():
     board = np.zeros((6,7))
@@ -32,9 +38,9 @@ def printBoard(board):
 
 #call function (Build_board)
 board = Build_board()
-printBoard(board)
+# printBoard(board)
 
-
+################ function to check winning###################
 def winning_move(board,piece):
     for c in range(COLUMNS-3):
         for r in range (ROWS):
@@ -57,38 +63,72 @@ def winning_move(board,piece):
         for r in range (ROWS-3):
           if board[r][c] == piece and board [r-1][c+1] == piece and board [r-2][c+2] == piece and board[r-3][c+3]:
                 return True
+################ function to check winning###################
+
+
+
+
+def draw(board):
+    for col in range(COLUMNS):
+        for row in range(ROWS):
+            pygame.draw.rect(screen,blue,(col*SIZEOFSQUARE,row*SIZEOFSQUARE+SIZEOFSQUARE,SIZEOFSQUARE,SIZEOFSQUARE))
+            pygame.draw.circle(screen,black,(int(col*SIZEOFSQUARE+SIZEOFSQUARE/2),int(row*SIZEOFSQUARE+SIZEOFSQUARE+SIZEOFSQUARE/2)),radius)
 
 switch=0 #to turn play to another player
 GameOver = False
+
+########pygame utilities###################
+pygame.init()
+SIZEOFSQUARE = 100
+width = COLUMNS*SIZEOFSQUARE
+height = (ROWS+1) * SIZEOFSQUARE
+size = (width,height)
+radius = int(SIZEOFSQUARE/2 -5)
+screen = pygame.display.set_mode(size)
+draw(board)
+pygame.display.update()
+
+########pygame utilities###################
+
+
+
+
+
 while  (GameOver==False):
 
-
-    #Player 1 play:
-    if switch == 0:
-        select = int(input("Select (player 1) from 0-6: "))
-        switch = 1
-        if isValid(board,select):
-            row =GetNextRow(board,select)
-            SetPiece(board,row,select,1)
-            if winning_move(board,1):
-                print("Player 1 wins ")
-                GameOver=True
-
-        # print(select)
-
-    # Player 2 play:
-    else:
-        select = int(input("Select (player 2) from 0-6: "))
-        switch = 0
-        if isValid(board,select):
-            row = GetNextRow(board,select)
-            SetPiece(board,row,select,2)
-            if winning_move(board, 2):
-                print("Player 2 wins ")
-                GameOver = True
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: ## 3shan lma ndos 3la X y5rgny mn el window
+            sys.exit()
 
 
-    printBoard(board)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            continue
+            # #Player 1 play:
+            # if switch == 0:
+            #     select = int(input("Select (player 1) from 0-6: "))
+            #     switch = 1
+            #     if isValid(board,select):
+            #         row =GetNextRow(board,select)
+            #         SetPiece(board,row,select,1)
+            #         if winning_move(board,1):
+            #             print("Player 1 wins ")
+            #             GameOver=True
+            #
+            #     # print(select)
+            #
+            # # Player 2 play:
+            # else:
+            #     select = int(input("Select (player 2) from 0-6: "))
+            #     switch = 0
+            #     if isValid(board,select):
+            #         row = GetNextRow(board,select)
+            #         SetPiece(board,row,select,2)
+            #         if winning_move(board, 2):
+            #             print("Player 2 wins ")
+            #             GameOver = True
+            #
+            # printBoard(board)
+
 
 
 
